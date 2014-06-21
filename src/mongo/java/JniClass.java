4e348f5ee9433c9c18d0c87bfdbb86a0b8c48673
katyusa <k.katalin.fazekas@gmail.com>
2014-06-21 17:04:49 +0200

    Java side of the demonstration
    implemented some collection creation method for the demo
    (createMongoDemo_2_DB(), createMongoDemo_3_DB())

--- src/mongo/java/JniClass.java
+++ src/mongo/java/JniClass.java
@@ -1,4 +1,6 @@
 
+import java.text.DecimalFormat;
+
 import javax.script.Invocable;
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
@@ -9,22 +11,28 @@ public class JniClass {
 	static final ScriptEngine engine = new ScriptEngineManager().getEngineByName("nashorn");
 	static final Invocable invocable = (Invocable) engine;
 	static final String debugTag = "JAVA-VM MESSAGE: ";
-  static final String errorReturn = "__errorInFunctionInvocation";
+	static final String errorReturn = "__errorInFunctionInvocation";
+	
 	static {
 		try {
 			engine.eval("var Mongo = Java.type('MongoManager')");
+			engine.eval("var AI = Java.type('java.util.concurrent.atomic.LongAdder');");
+			engine.eval("var Random = Java.type('java.util.Random')");
+			engine.eval("DoubleStream = Java.type('java.util.stream.DoubleStream')");
+			engine.eval("var Array = Java.type('java.util.Arrays')");
+			engine.eval("var mongo = new Mongo();");
 		} catch (ScriptException e1) {
-      System.out.println(debugTag + "ScriptExceptionError in Mongo type creation: (JniClass:line17): " + e1.getMessage());
+      System.out.println(debugTag + "ScriptExceptionError in Mongo type creation: (JniClass:line16): " + e1.getMessage());
 		} catch (Exception e2) {
-			System.out.println(debugTag + "ExceptionError in Mongo type creation: (JniClass:line19): " + e2.getMessage());
+			System.out.println(debugTag + "ExceptionError in Mongo type creation: (JniClass:line16): " + e2.getMessage());
 		}
 	}
 	
-	//public java.lang.String invokeFunctionNashorn(java.lang.String);
-	//descriptor: (Ljava/lang/String;)Ljava/lang/Object;
+	/*  
+	*	public java.lang.Object invokeFunctionNashorn(java.lang.String);
+    *	descriptor: (Ljava/lang/String;)Ljava/lang/Object;
+    */
 	public Object invokeFunctionNashorn(String code) {	
-		Object result = null;
-		JOptionPane.showMessageDialog(null, code, "Code", JOptionPane.OK_CANCEL_OPTION);
 		System.out.println(debugTag + "invokeFunctionNashorn starts with the following argument: \n" + code +"\n");
 		
 		System.out.println(debugTag + "engine.eval() starts.");
@@ -32,29 +40,35 @@ public class JniClass {
 			engine.eval(code);
 			System.out.println(debugTag + "engine.eval() is done.");
 		} catch (ScriptException e1) {
-			System.out.println(debugTag + "ScriptExceptionError in engine eval(code): (JniClass:line35): " + e1.getMessage());
+			System.out.println(debugTag + "ScriptExceptionError in engine eval(code): (JniClass:line33): " + e1.getMessage());
 		} catch (Exception e2) {
-			System.out.println(debugTag + "ExceptionError in engine eval(code): (JniClass:line37): " + e2.getMessage());
+			System.out.println(debugTag + "ExceptionError in engine eval(code): (JniClass:line33): " + e2.getMessage());
 		}
 		
-	
-		
+		long startTime, endTime, duration = 0;
 		try {
-			Object part_res = invocable.invokeFunction("_funcs"); 
+			startTime = System.nanoTime();
+			Object part_res = invocable.invokeFunction("_funcs");
+			endTime = System.nanoTime();
+			duration = endTime - startTime;
 			if (part_res != null) { 
-				System.out.println(debugTag + "invokeFunctionNashorn method returns with valid result: \n" + part_res.toString() + "\n\n"); 
+				JOptionPane.showMessageDialog(null, part_res, "Result of the evaluation", JOptionPane.OK_CANCEL_OPTION);
+				System.out.println(debugTag + "invokeFunctionNashorn method returns with valid result: \n" + part_res.toString() + "\n\n");
+				double dur_inSeconds = ((double)duration / 1000000000);
+				System.out.println(debugTag + "runTime: " + new DecimalFormat("##.####").format(dur_inSeconds) + " sec"); 
+				
 				return part_res.toString();
 			}
-			
 		} catch (NoSuchMethodException e) {
-			System.out.println(debugTag + "NoSuchMethodExceptionError in invoke _funcs: (JniClass:line50)" + e.getMessage());
+			System.out.println(debugTag + "NoSuchMethodExceptionError in invoke _funcs: (JniClass:line42)" + e.getMessage());
 		} catch (ScriptException e) {
-			System.out.println(debugTag + "ScriptExceptionError in invoke _funcs: (JniClass:line52): " + e.getMessage());
-		} catch (Exception e2) {
-			System.out.println(debugTag + "ExceptionError in invoke _funcs: (JniClass:line54): " + e2.getMessage());
+			System.out.println(debugTag + "ScriptExceptionError in invoke _funcs: (JniClass:line42): " + e.getMessage());
+		} catch (Exception e) {
+			System.out.println(debugTag + "ExceptionError in invoke _funcs: (JniClass:line42): " + e.getMessage());
 		}
 		
-		
+		double dur_inSeconds = ((double)duration / 1000000000);
+		System.out.println(debugTag + "runTime: " + new DecimalFormat("##.####").format(dur_inSeconds) + " sec"); 
 		System.out.println(debugTag + "invokeFunctionNashorn method returns with \"" + errorReturn + "\".\n\n");
 		return errorReturn;
 
--- src/mongo/java/MongoManager.java
+++ src/mongo/java/MongoManager.java
@@ -1,7 +1,11 @@
 import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.Random;
 import java.util.Set;
 
 import com.mongodb.BasicDBObject;
@@ -31,6 +35,14 @@ public class MongoManager {
 		coll = db.getCollection("test");
 	}
 	
+	public void selectCollection(String collName) {
+		coll = db.getCollection(collName);
+	}
+  
+	public void getDB(String dbName) {
+		db = mongoClient.getDB(dbName);
+	}
+  
 	
 	public void save(Map entry) {
 		BasicDBObject doc = new BasicDBObject();
@@ -61,25 +73,72 @@ public class MongoManager {
 			return cursor.next();
 		}
 		
-		System.out.println(debugTag + "findOne() query has no result.");
+		System.out.println(debugTag + "findOne() query has no result with the following query: " + searchQuery);
 		return null;
 	}
+  
+	public ArrayList<Object> find(Map entry) {
+		BasicDBObject searchQuery = new BasicDBObject();
+		Iterator entries = entry.entrySet().iterator();
+		while(entries.hasNext()) {
+			Entry thisEntry = (Entry) entries.next();
+			Object key = thisEntry.getKey();
+			Object value = thisEntry.getValue();
+			//System.out.println(key + " -> " + value);
+			searchQuery.append(key.toString(), value);
+		}
+		ArrayList<Object> result = new ArrayList<Object>();
+		DBCursor cursor = coll.find(searchQuery);
+		while (cursor.hasNext()) {
+			result.add(cursor.next());
+		}
+
+		return result;
+	}
 	
+	public ArrayList<Object> find(Map entry, String coll_name) {
+		coll = db.getCollection(coll_name);
+		BasicDBObject searchQuery = new BasicDBObject();
+		Iterator entries = entry.entrySet().iterator();
+		while(entries.hasNext()) {
+			Entry thisEntry = (Entry) entries.next();
+			Object key = thisEntry.getKey();
+			Object value = thisEntry.getValue();
+			//System.out.println(key + " -> " + value);
+			searchQuery.append(key.toString(), value);
+		}
+		ArrayList<Object> result = new ArrayList<Object>();
+		DBCursor cursor = coll.find(searchQuery);
+		while (cursor.hasNext()) {
+			result.add(cursor.next());
+		}
+
+		return result;
+	}
 	
-	public void printCollection() {
-		System.out.println(debugTag + "printCollection() starts.");
-		
-		try {
-			mongoClient = new MongoClient();
-			System.out.println(debugTag + "MongoClient created");
-		} catch (UnknownHostException uh_exc) {
-			System.out.println(debugTag + "UnknownHostExceptionError in mongoClient creation: (MongoManager:line76): " + uh_exc.getMessage());
+	public List<Object> getArrayData(Map entry) {
+		coll = db.getCollection("mongoDemo_2");
+		BasicDBObject searchQuery = new BasicDBObject();
+		Iterator entries = entry.entrySet().iterator();
+		while(entries.hasNext()) {
+			Entry thisEntry = (Entry) entries.next();
+			Object ekey = thisEntry.getKey();
+			Object value = thisEntry.getValue();
+			//System.out.println(key + " -> " + value);
+			searchQuery.append(ekey.toString(), value);
 		}
 		
-		db = mongoClient.getDB("test");
-		coll = db.getCollection("test");
-		
-		
+		DBCursor cursor = coll.find(searchQuery);
+		if (cursor.hasNext()) {
+			return Arrays.asList(cursor.next().get("value"));
+		}
+
+		return new ArrayList<Object>();
+	}
+	
+	public void printCollectionNames() {
+		System.out.println(debugTag + "printCollectionNames() starts.");
+
 		try {
 			Set<String> colls = db.getCollectionNames();
 			
@@ -91,6 +150,35 @@ public class MongoManager {
 			System.out.println(debugTag + "MongoException in getCollectionNames(): (MongoManager:line91): " + mng_exc.getMessage());
 		}
 
-		System.out.println(debugTag + "printCollection() done."); 
+		System.out.println(debugTag + "printCollectionNames() done."); 
 	}
+	
+	public void createMongoDemo_2_DB(int size) {
+		coll = db.getCollection("mongoDemo_2");
+		 
+		ArrayList<Integer> numbers = new ArrayList<Integer>();
+		for(int i = 1; i <= size; i++) {
+			numbers.add(i);
+		}
+		BasicDBObject arrayEntry = new BasicDBObject();
+		arrayEntry.append("name", "Numbers");
+		arrayEntry.append("value", numbers);
+		coll.insert(arrayEntry);
+	}
+	
+	public void createMongoDemo_3_DB() {
+		coll = db.getCollection("mongoDemo_3");
+		Random randomGenerator = new Random();
+		for(int i=0; i < 10; i++) {
+			BasicDBObject entry = new BasicDBObject();
+			
+			entry.append("name", ("name" + i));
+			entry.append("age", randomGenerator.nextInt(99)+1);
+			String gender = randomGenerator.nextBoolean() ? "male" : "female"; 
+			entry.append("gender", gender);
+				
+			coll.save(entry);
+		}
+	}
+	
 }
